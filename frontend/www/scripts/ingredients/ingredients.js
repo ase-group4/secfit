64f7e4b6b61b87f69a8c1b9b511c0a47f3b29b00
hermannm <hermann.morkrid@gmail.com>
2022-03-08 02:49:17 +0100

    add ingredients page and script

---
+++ frontend/www/scripts/ingredients/ingredients.js
@@ -0,0 +1,63 @@
+// When the DOM loads, populates the ingredient overview and adds event listeners.
+window.addEventListener("DOMContentLoaded", () => {
+	refreshIngredients();
+
+	const ingredientSubmitButton = document.querySelector("#ingredient-submit-button");
+	ingredientSubmitButton.addEventListener("click", refreshIngredients);
+});
+
+/** Fetches ingredients from the backend and updates the ingredient overview. */
+async function refreshIngredients() {
+	const ingredients = await fetchIngredients();
+
+	if (ingredients === undefined) {
+		return;
+	}
+
+	updateIngredientList(ingredients);
+}
+
+/**
+ * Sends a request to the backend to get all ingredients, and returns them.
+ * If backend returns an error, shows it at the top of the page.
+ */
+async function fetchIngredients() {
+	const response = await sendRequest("GET", `${HOST}/api/ingredients`);
+	const data = await response.json();
+
+	if (!response.ok) {
+		const alert = createAlert("Could not fetch ingredients!", data);
+		document.body.prepend(alert);
+		return;
+	}
+
+	return data.results;
+}
+
+/** Takes a list of ingredients and populates the ingredient overview page with them. */
+function updateIngredientList(ingredients) {
+	const container = document.querySelector("#div-content");
+	while (container.firstChild) {
+		container.removeChild(container.lastChild);
+	}
+
+	const ingredientTemplate = document.querySelector("#template-ingredient");
+
+	for (const ingredient of ingredients) {
+		const ingredientElement = ingredientTemplate.content.firstElementChild.cloneNode(true);
+
+		const header = ingredientElement.querySelector("h5");
+		header.textContent = ingredient.name;
+
+		const publisherField = ingredientElement.querySelector("#publisher-field");
+		publisherField.textContent += ingredient.publisher_name;
+
+		const tableRows = ingredientElement.querySelector("table").querySelectorAll("tr");
+		tableRows[0].querySelectorAll("td")[1].textContent = `${ingredient.protein} g`;
+		tableRows[1].querySelectorAll("td")[1].textContent = `${ingredient.fat} g`;
+		tableRows[2].querySelectorAll("td")[1].textContent = `${ingredient.carbohydrates} g`;
+		tableRows[3].querySelectorAll("td")[1].textContent = `${ingredient.calories} kcal`;
+
+		container.appendChild(ingredientElement);
+	}
+}
