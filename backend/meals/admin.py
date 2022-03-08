c3dd84a9130e9f2d3097bf0e9ff22a1d34dd0427
hermannm <hermann.morkrid@gmail.com>
2022-03-08 02:46:52 +0100

    add ingredients to django admin panel

--- backend/meals/admin.py
+++ backend/meals/admin.py
@@ -1,6 +1,7 @@
 """Module for registering models from meals app so they appear on the admin page."""
 from django.contrib import admin
-from .models import Meal, MealFile
+from .models import Ingredient, Meal, MealFile
 
 admin.site.register(Meal)
+admin.site.register(Ingredient)
 admin.site.register(MealFile)
