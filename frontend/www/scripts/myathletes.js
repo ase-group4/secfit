b94a65e0e9fe2a9f20c89d2ea2b98a07bc56f717
Jorunn <jorunn@selbu.org>
2022-03-09 01:11:43 +0100

    adds search for athletes

--- frontend/www/scripts/myathletes.js
+++ frontend/www/scripts/myathletes.js
@@ -28,9 +28,13 @@ async function displayCurrentRoster() {
         }
     }
 
+    const usersHTML = await fetchUsers(currentUser)
+
     let emptyClone = templateEmptyAthlete.content.cloneNode(true);
     let emptyDiv = emptyClone.querySelector("div");
     let emptyButton = emptyDiv.querySelector("button");
+    let userlist = emptyDiv.querySelector("datalist")
+    userlist.innerHTML=usersHTML
     emptyButton.addEventListener("click", addAthleteRow);
     controls.appendChild(emptyDiv);
 }
@@ -49,6 +53,25 @@ function createFilledRow(templateFilledAthlete, inputValue, controls, disabled) 
         filledButton.disabled = true;
     }
     controls.appendChild(filledDiv);
+}
+
+async function fetchUsers(currentUser) {
+    let response = await sendRequest("GET", `${HOST}/api/users/`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+        let data = await response.json();
+
+        users = data.results;
+        innerHTML = ""
+        for(let j = 0; j < users.length; j++) {
+            user = users[j];
+            if (user.id != currentUser.id){
+                innerHTML += `<option value=${user.username}> </option>`
+            }
+        }
+        return innerHTML
+    }
 }
 
 async function displayFiles() {
