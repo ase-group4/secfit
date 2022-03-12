21217542ebfaf50322d9f18911c6248b69d6591f
hermannm <hermann.morkrid@gmail.com>
2022-03-12 16:00:39 +0100

    apply formatter

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -9,14 +9,14 @@ from rest_framework.test import APIRequestFactory
 from rest_framework.request import Request
 
 
-
 class TestUserSerializer(TestCase):
     """
     Class for statement coverage of User Serializer.
     """
+
     def setUp(self):
         self.user_attributes = {
-            "email" : "user@mail.us",
+            "email": "user@mail.us",
             "password": "password",
             "phone_number": "12345678",
             "country": "Norway",
@@ -34,78 +34,100 @@ class TestUserSerializer(TestCase):
             owner=self.user,
             date="2022-03-03T18:14:00Z",
             notes="note",
-            visibility="PU"
+            visibility="PU",
         )
 
         factory = APIRequestFactory()
-        request = factory.get('/')
-        self.context = {'request': Request(request)}
-        self.serializer = UserSerializer(instance=self.user, context = self.context)
-    
+        request = factory.get("/")
+        self.context = {"request": Request(request)}
+        self.serializer = UserSerializer(instance=self.user, context=self.context)
+
     def test_contains_expected_fields(self):
-        """ Tests that all expected data fields are present after using the UserSerializer()-function. """
+        """
+        Tests that all expected data fields are present
+        after using the UserSerializer() function.
+        """
+
         data = self.serializer.data
-        self.assertEqual(set(data.keys()), set(['url', 'id', 'email', 'username', 'athletes', 'phone_number', 'country', 'city', 'street_address', 'coach', 'workouts', 'coach_files', 'athlete_files']))
+        self.assertEqual(
+            set(data.keys()),
+            set(
+                [
+                    "url",
+                    "id",
+                    "email",
+                    "username",
+                    "athletes",
+                    "phone_number",
+                    "country",
+                    "city",
+                    "street_address",
+                    "coach",
+                    "workouts",
+                    "coach_files",
+                    "athlete_files",
+                ]
+            ),
+        )
 
     def test_url_field_content(self):
-        """ Tests that url field has expected content. """
+        """Tests that url field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['url'], 'http://testserver/api/users/3/')
+        self.assertEqual(data["url"], "http://testserver/api/users/3/")
 
     def test_id_field_content(self):
-        """ Tests that id field has expected content. """
+        """Tests that id field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['id'], 3)
+        self.assertEqual(data["id"], 3)
 
     def test_email_field_content(self):
-        """ Tests that email field has expected content. """
+        """Tests that email field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['email'], self.user_attributes['email'])
+        self.assertEqual(data["email"], self.user_attributes["email"])
 
     def test_username_field_content(self):
-        """ Tests that username field has expected content. """
+        """Tests that username field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['username'], "user")
-    
+        self.assertEqual(data["username"], "user")
+
     def test_phone_number_field_content(self):
-        """ Tests that phone_number field has expected content. """
+        """Tests that phone_number field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['phone_number'], self.user_attributes['phone_number'])
+        self.assertEqual(data["phone_number"], self.user_attributes["phone_number"])
 
     def test_country_field_content(self):
-        """ Tests that country field has expected content. """
+        """Tests that country field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['country'], self.user_attributes['country'])
+        self.assertEqual(data["country"], self.user_attributes["country"])
 
     def test_city_field_content(self):
-        """ Tests that city field has expected content. """
+        """Tests that city field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['city'], self.user_attributes['city'])
+        self.assertEqual(data["city"], self.user_attributes["city"])
 
     def test_street_address_field_content(self):
-        """ Tests that street_adress field has expected content. """
+        """Tests that street_adress field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['street_address'], self.user_attributes['street_address'])
+        self.assertEqual(data["street_address"], self.user_attributes["street_address"])
 
     def test_athletes_field_content(self):
-        """ Tests that athletes field has expected content. """
+        """Tests that athletes field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['athletes'], ['http://testserver/api/users/2/'])
-    
+        self.assertEqual(data["athletes"], ["http://testserver/api/users/2/"])
+
     def test_coach_field_content(self):
-        """ Tests that coach field has expected content. """
+        """Tests that coach field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['coach'], 'http://testserver/api/users/1/')
-    
+        self.assertEqual(data["coach"], "http://testserver/api/users/1/")
+
     def test_workouts_field_content(self):
-        """ Tests that workouts field has expected content. """
+        """Tests that workouts field has expected content."""
         data = self.serializer.data
-        self.assertEqual(data['workouts'], ['http://testserver/api/workouts/1/'])
-    
+        self.assertEqual(data["workouts"], ["http://testserver/api/workouts/1/"])
+
     def tests_validate_password(self):
-        """ UserSerializer.validate_password() validates and returnes new password """
+        """UserSerializer.validate_password() validates and returnes new password"""
         new_password = "new_password"
         self.user.set_password(new_password)
         user_serializer = UserSerializer(self.user, context=self.context)
         self.assertEqual(user_serializer.validate_password(new_password), new_password)
-
