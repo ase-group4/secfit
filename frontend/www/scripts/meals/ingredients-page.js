bca0d8ee1eb6ddbc2bd8b09acef1696d73fe1d92
hermannm <hermann.morkrid@gmail.com>
2022-03-12 05:47:43 +0100

    update ingredients page

--- frontend/www/scripts/meals/ingredients-page.js
+++ frontend/www/scripts/meals/ingredients-page.js
@@ -43,13 +43,13 @@ function updateIngredientList(ingredients) {
     header.textContent = ingredient.name;
 
     const publisherField = ingredientElement.querySelector("#publisher-field");
-    publisherField.textContent += ingredient.publisher_name;
+    publisherField.textContent += ` ${ingredient.publisher_name}`;
 
     const tableRows = ingredientElement.querySelector("table").querySelectorAll("tr");
-    tableRows[0].querySelectorAll("td")[1].textContent = `${ingredient.protein} g`;
+    tableRows[0].querySelectorAll("td")[1].textContent = `${ingredient.calories} kcal`;
     tableRows[1].querySelectorAll("td")[1].textContent = `${ingredient.fat} g`;
     tableRows[2].querySelectorAll("td")[1].textContent = `${ingredient.carbohydrates} g`;
-    tableRows[3].querySelectorAll("td")[1].textContent = `${ingredient.calories} kcal`;
+    tableRows[3].querySelectorAll("td")[1].textContent = `${ingredient.protein} g`;
 
     container.appendChild(ingredientElement);
   }
