bbf09a9c993e11eac132a9186885959cf52aa92d
hermannm <hermann.morkrid@gmail.com>
2022-03-12 03:41:32 +0100

    improve structure of meals-page

--- frontend/www/scripts/meals/meals-page.js
+++ frontend/www/scripts/meals/meals-page.js
@@ -1,62 +1,12 @@
 import { calculateNutritionTotals } from "./calculate-nutrition.js";
 
-async function fetchMeals(ordering) {
-  let response = await sendRequest("GET", `${HOST}/api/meals/?ordering=${ordering}`);
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  } else {
-    let data = await response.json();
-
-    let meals = data.results;
-    let container = document.getElementById("div-content");
-    meals.forEach((meal) => {
-      let templateMeal = document.querySelector("#template-meal");
-      let cloneMeal = templateMeal.content.cloneNode(true);
-
-      let aMeal = cloneMeal.querySelector("a");
-      aMeal.href = `meal.html?id=${meal.id}`;
-
-      let h5 = aMeal.querySelector("h5");
-      h5.textContent = meal.name;
-
-      let localDate = new Date(meal.date);
-
-      const leftTable = aMeal.querySelector(".left-table");
-      const leftRows = leftTable.querySelectorAll("tr");
-      leftRows[0].querySelectorAll("td")[1].textContent = localDate.toLocaleDateString(); // Date
-      leftRows[1].querySelectorAll("td")[1].textContent = localDate.toLocaleTimeString(); // Time
-      leftRows[2].querySelectorAll("td")[1].textContent = meal.owner_username; //Owner
-
-      const { calories, protein, fat, carbohydrates } = calculateNutritionTotals(
-        meal.ingredient_weights
-      );
-
-      const rightTable = aMeal.querySelector(".right-table");
-      const rightRows = rightTable.querySelectorAll("tr");
-      rightRows[0].querySelectorAll("td")[1].textContent = calories;
-      rightRows[1].querySelectorAll("td")[1].textContent = protein;
-      rightRows[2].querySelectorAll("td")[1].textContent = fat;
-      rightRows[3].querySelectorAll("td")[1].textContent = carbohydrates;
-
-      container.appendChild(aMeal);
-    });
-    return meals;
-  }
-}
-
-function createMeal() {
-  window.location.replace("meal.html");
-}
-
 window.addEventListener("DOMContentLoaded", async () => {
   let createButton = document.querySelector("#btn-create-meal");
   createButton.addEventListener("click", createMeal);
-  let ordering = "-date";
 
+  let ordering = "-date";
   const urlParams = new URLSearchParams(window.location.search);
   if (urlParams.has("ordering")) {
-    let aSort = null;
     ordering = urlParams.get("ordering");
     if (ordering == "name" || ordering == "owner" || ordering == "date") {
       let aSort = document.querySelector(`a[href="?ordering=${ordering}"`);
@@ -103,3 +53,52 @@ window.addEventListener("DOMContentLoaded", async () => {
     });
   }
 });
+
+function createMeal() {
+  window.location.replace("meal.html");
+}
+
+async function fetchMeals(ordering) {
+  let response = await sendRequest("GET", `${HOST}/api/meals/?ordering=${ordering}`);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
+    let data = await response.json();
+
+    let meals = data.results;
+    let container = document.getElementById("div-content");
+    meals.forEach((meal) => {
+      let templateMeal = document.querySelector("#template-meal");
+      let cloneMeal = templateMeal.content.cloneNode(true);
+
+      let aMeal = cloneMeal.querySelector("a");
+      aMeal.href = `meal.html?id=${meal.id}`;
+
+      let h5 = aMeal.querySelector("h5");
+      h5.textContent = meal.name;
+
+      let localDate = new Date(meal.date);
+
+      const leftTable = aMeal.querySelector(".left-table");
+      const leftRows = leftTable.querySelectorAll("tr");
+      leftRows[0].querySelectorAll("td")[1].textContent = localDate.toLocaleDateString(); // Date
+      leftRows[1].querySelectorAll("td")[1].textContent = localDate.toLocaleTimeString(); // Time
+      leftRows[2].querySelectorAll("td")[1].textContent = meal.owner_username; //Owner
+
+      const { calories, protein, fat, carbohydrates } = calculateNutritionTotals(
+        meal.ingredient_weights
+      );
+
+      const rightTable = aMeal.querySelector(".right-table");
+      const rightRows = rightTable.querySelectorAll("tr");
+      rightRows[0].querySelectorAll("td")[1].textContent = calories;
+      rightRows[1].querySelectorAll("td")[1].textContent = protein;
+      rightRows[2].querySelectorAll("td")[1].textContent = fat;
+      rightRows[3].querySelectorAll("td")[1].textContent = carbohydrates;
+
+      container.appendChild(aMeal);
+    });
+    return meals;
+  }
+}
