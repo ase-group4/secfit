fcf1a35723be9e33ff265b6c863eef2f6f78036c
hermannm <hermann.morkrid@gmail.com>
2022-03-18 10:55:46 +0100

    apply formatter and fix typos

--- frontend/cypress/integration/blackbox/workouts.spec.js
+++ frontend/cypress/integration/blackbox/workouts.spec.js
@@ -1,218 +1,215 @@
-import 'cypress-file-upload';
-import Chance from 'chance';
+import "cypress-file-upload";
+import Chance from "chance";
 const chance = new Chance();
 
-
-function addWorkout (name, visibility) {
-    cy.visit(WORKOUT_URL)
-    cy.get('input[name="name"]').type(name);
-    cy
-    .get('input[name="date"]')
+function addWorkout(name, visibility) {
+  cy.visit(WORKOUT_URL);
+  cy.get('input[name="name"]').type(name);
+  cy.get('input[name="date"]')
     .click()
-    .then(input => {
-        input[0].dispatchEvent(new Event('input', { bubbles: true }))
-        input.val('2017-04-30T13:00:00')
+    .then((input) => {
+      input[0].dispatchEvent(new Event("input", { bubbles: true }));
+      input.val("2017-04-30T13:00:00");
     })
-    .click()
-    cy.get('textarea[name="notes"]').type('This is a note.');
-    cy.get('#inputVisibility').select(visibility);
-    cy.get('input[name="files"]').attachFile('images/bugs.jpg');
-    cy.get('#btn-ok-workout').click();
-    cy.wait(1000)
-    goToWorkout(name)
-    addComment()
+    .click();
+  cy.get('textarea[name="notes"]').type("This is a note.");
+  cy.get("#inputVisibility").select(visibility);
+  cy.get('input[name="files"]').attachFile("images/bugs.jpg");
+  cy.get("#btn-ok-workout").click();
+  cy.wait(1000);
+  goToWorkout(name);
+  addComment();
 }
 
-function addComment () {
-    cy.get('#comment-area').type("This is a comment.")
-    cy.get('#post-comment').click()
+function addComment() {
+  cy.get("#comment-area").type("This is a comment.");
+  cy.get("#post-comment").click();
 }
 
 function goToWorkout(name) {
-    cy.visit(WORKOUTS_URL)
-    cy.wait(1000)
-    cy.contains(name).click()
-    cy.wait(500)
+  cy.visit(WORKOUTS_URL);
+  cy.wait(1000);
+  cy.contains(name).click();
+  cy.wait(500);
 }
 
-function addUser (username, password) {
-    cy.visit(REGISTER_URL)
-    cy.get('input[name="username"]').type(username);
-    cy.get('input[name="password"]').type(password);
-    cy.get('input[name="password1"]').type(password);
-    cy.get('#btn-create-account').click();
-    cy.wait(1000)
+function addUser(username, password) {
+  cy.visit(REGISTER_URL);
+  cy.get('input[name="username"]').type(username);
+  cy.get('input[name="password"]').type(password);
+  cy.get('input[name="password1"]').type(password);
+  cy.get("#btn-create-account").click();
+  cy.wait(1000);
 }
 
 function login(username, password) {
-    cy.visit(LOGOUT_URL)
-    cy.wait(500)
-    cy.visit(LOGIN_URL)
-    cy.get('input[name="username"]').type(username);
-    cy.get('input[name="password"]').type(password);
-    cy.get('#btn-login').click();
-    cy.wait(1000)
+  cy.visit(LOGOUT_URL);
+  cy.wait(500);
+  cy.visit(LOGIN_URL);
+  cy.get('input[name="username"]').type(username);
+  cy.get('input[name="password"]').type(password);
+  cy.get("#btn-login").click();
+  cy.wait(1000);
 }
 
-function addCoach (username) {
-    cy.visit(COACH_URL)
-    cy.wait(500)
-    cy.get('#button-edit-coach').click();
-    cy.get('input[name="coach"]').type(username)
-    cy.get('#button-set-coach').click();
+function addCoach(username) {
+  cy.visit(COACH_URL);
+  cy.wait(500);
+  cy.get("#button-edit-coach").click();
+  cy.get('input[name="coach"]').type(username);
+  cy.get("#button-set-coach").click();
 }
 
-const LOGIN_URL = '../../www/login.html'
-const REGISTER_URL = '../../www/register.html'
-const LOGOUT_URL = '../../www/logout.html'
-const WORKOUTS_URL = '../../www/workouts.html'
-const WORKOUT_URL = '../../www/workout.html'
-const COACH_URL = '../../www/mycoach.html'
+const LOGIN_URL = "../../www/login.html";
+const REGISTER_URL = "../../www/register.html";
+const LOGOUT_URL = "../../www/logout.html";
+const WORKOUTS_URL = "../../www/workouts.html";
+const WORKOUT_URL = "../../www/workout.html";
+const COACH_URL = "../../www/mycoach.html";
 
+describe("Workouts visibility", () => {
+  const user = chance.first() + chance.last().replace(/[^a-zA-Z0-9]/g, "-");
+  const user_password = chance.string({ length: 10, pool: "abcd" });
 
-describe('Workouts visibility', () => {
-    const user = chance.first()+chance.last().replace(/[^a-zA-Z0-9]/g, '-')
-    const user_password = chance.string({length: 10, pool: 'abcd'})
+  const coach = chance.first() + chance.last().replace(/[^a-zA-Z0-9]/g, "-");
+  const coach_password = chance.string({ length: 10, pool: "abcd" });
 
-    const coach = chance.first()+chance.last().replace(/[^a-zA-Z0-9]/g, '-')
-    const coach_password = chance.string({length: 10, pool: 'abcd'})
+  const athlete_password = chance.string({ length: 10, pool: "abcd" });
+  const athlete = chance.first() + chance.last().replace(/[^a-zA-Z0-9]/g, "-");
 
-    const athlete_password = chance.string({length: 10, pool: 'abcd'})
-    const athlete = chance.first()+chance.last().replace(/[^a-zA-Z0-9]/g, '-')
+  const workout_PU = "Workout-PU-" + athlete;
+  const workout_PR = "Workout-PR-" + athlete;
+  const workout_CO = "Workout-CO-" + athlete;
 
-    const workout_PU = "Workout-PU-" + athlete
-    const workout_PR = "Workout-PR-" + athlete
-    const workout_CO = "Workout-CO-" + athlete
+  function testCanNotViewDetails() {
+    cy.get(".alert").should("be.visible");
+    cy.get(".alert").contains("Could not retrieve workout data!");
+    cy.get(".alert").contains("You do not have permission to perform this action.");
+    cy.get('input[name="name"]').should("have.value", "");
+    cy.get('input[name="date"]').should("have.value", "");
+    cy.get('textarea[name="notes"]').should("have.value", "");
+    cy.get("#inputVisibility").should("have.value", "PU"); //value is set to PU when no other is chosen
+    cy.get("#inputOwner").should("have.value", "");
+    cy.get(".comment-wrapper").contains("This is a comment.").should("not.exist");
+    cy.get("#btn-gallery-workout").click();
+    cy.wait(500);
+    cy.get(".main-img").find("img").should("have.attr", "src").should("not.include", "bugs.jpg");
+  }
 
-    function testCanNotViewDetails(){
-        cy.get('.alert').should('be.visible');
-        cy.get('.alert').contains('Could not retrieve workout data!')
-        cy.get('.alert').contains('You do not have permission to perform this action.')
-        cy.get('input[name="name"]').should("have.value", '');
-        cy.get('input[name="date"]').should("have.value", '');
-        cy.get('textarea[name="notes"]').should("have.value", '');
-        cy.get('#inputVisibility').should("have.value", "PU"); //value is set to PU when no other is chosen
-        cy.get('#inputOwner').should("have.value", '');
-        cy.get('.comment-wrapper').contains('This is a comment.').should('not.exist');
-        cy.get('#btn-gallery-workout').click();
-        cy.wait(500)
-        cy.get(".main-img").find('img').should('have.attr', 'src').should('not.include','bugs.jpg')
-    }
+  function testCanViewDetails(workout, visibility) {
+    cy.get('input[name="name"]').should("have.value", workout);
+    cy.get('input[name="date"]').should("have.value", "2017-04-30T13:00:00");
+    cy.get('textarea[name="notes"]').should("have.value", "This is a note.");
+    cy.get("#inputVisibility").should("have.value", visibility);
+    cy.get("#inputOwner").should("have.value", athlete);
+    cy.get(".comment-wrapper").contains("This is a comment.");
+    cy.get("#btn-gallery-workout").click();
+    cy.wait(500);
+    cy.get(".main-img").find("img").should("have.attr", "src").should("include", "bugs.jpg");
+  }
 
-    function testCanViewDetails(workout, visibility){
-        cy.get('input[name="name"]').should("have.value", workout);
-        cy.get('input[name="date"]').should("have.value", '2017-04-30T13:00:00');
-        cy.get('textarea[name="notes"]').should("have.value", 'This is a note.');
-        cy.get('#inputVisibility').should("have.value", visibility);
-        cy.get('#inputOwner').should("have.value", athlete);
-        cy.get('.comment-wrapper').contains('This is a comment.');
-        cy.get('#btn-gallery-workout').click();
-        cy.wait(500)
-        cy.get(".main-img").find('img').should('have.attr', 'src').should('include','bugs.jpg')
-    }
+  before(() => {
+    addUser(coach, coach_password);
+    cy.visit(LOGOUT_URL);
+    cy.wait(500);
+    addUser(user, user_password);
+    cy.visit(LOGOUT_URL);
+    cy.wait(500);
+    addUser(athlete, athlete_password);
 
-    before(() => {
-        addUser(coach, coach_password)
-        cy.visit(LOGOUT_URL)
-        cy.wait(500)
-        addUser(user, user_password)
-        cy.visit(LOGOUT_URL)
-        cy.wait(500)
-        addUser(athlete, athlete_password)
+    addCoach(coach);
+    cy.wait(1000);
+    addWorkout(workout_PU, "Public");
+    addWorkout(workout_PR, "Private");
+    addWorkout(workout_CO, "Coach");
+  });
 
-        addCoach(coach)
-        cy.wait(1000)
-        addWorkout(workout_PU, "Public")
-        addWorkout(workout_PR, "Private")
-        addWorkout(workout_CO, "Coach")
-      })
+  describe("Athlete", function () {
+    it("may see their own public visibility excercise", () => {
+      login(athlete, athlete_password);
+      goToWorkout(workout_PU);
+      testCanViewDetails(workout_PU, "PU");
+    });
+    it("may see their own coach visibility excercise", () => {
+      login(athlete, athlete_password);
+      goToWorkout(workout_CO);
+      testCanViewDetails(workout_CO, "CO");
+    });
+    it("may see their own private visibility excercise", () => {
+      login(athlete, athlete_password);
+      goToWorkout(workout_PR);
+      testCanViewDetails(workout_PR, "PR");
+    });
+  });
 
-    describe('Athlete', function () {
-        it('may see their own public visibility excercise', () => {
-            login(athlete, athlete_password)
-            goToWorkout(workout_PU)
-            testCanViewDetails(workout_PU, "PU")
-        })
-        it('may see their own coach visibility excercise', () => {
-            login(athlete, athlete_password)
-            goToWorkout(workout_CO)
-            testCanViewDetails(workout_CO, "CO")
-        })
-        it('may see their own private visibility excercise', () => {
-            login(athlete, athlete_password)
-            goToWorkout(workout_PR)
-            testCanViewDetails(workout_PR, "PR")
-        })
-    })
-
-    describe('Coach', function () {
-        it('may see their athletes public visibility excercise', () => {
-            login(coach, coach_password)
-            goToWorkout(workout_PU)
-            testCanViewDetails(workout_PU, "PU")
-
-        })
-        it('may see their athletes coach visibility excercise', () => {
-            login(coach, coach_password)
-            goToWorkout(workout_CO)
-            testCanViewDetails(workout_CO, "CO")
-        })
-        /*This test is commented out, as it should pass, but due to pre-existing bugs it does not, and the task does not ask us to fix bugs we find in the original code.
-
-        it('may not see their athletes private visibility excercise', () => {
-            login(athlete, athlete_password)
-            goToWorkout(workout_PR)
-            cy.url().as('workoutUrl');
-            login(coach, coach_password)
-            cy.get('@workoutUrl').then(url => {
-                cy.window().then(win => {
-                    return win.open(url, '_self');
-                  });
-            })
-            testCanNotViewDetails()
-        })*/
-    })
-
-    describe('User', function () {
-        it('may see others public visibility excercise', () => {
-            login(user, user_password)
-            goToWorkout(workout_PU)
-            testCanViewDetails(workout_PU, "PU")
-
-        })
-        it('may not see others coach visibility excercise', () => {
-            cy.on('uncaught:exception', (err, runnable) => {
-                expect(err.message).to.include("Cannot read properties of null (reading 'owner')")
-                return false
+  describe("Coach", function () {
+    it("may see their athletes public visibility excercise", () => {
+      login(coach, coach_password);
+      goToWorkout(workout_PU);
+      testCanViewDetails(workout_PU, "PU");
+    });
+    it("may see their athletes coach visibility excercise", () => {
+      login(coach, coach_password);
+      goToWorkout(workout_CO);
+      testCanViewDetails(workout_CO, "CO");
+    });
+    /*
+    This test is commented out, as it should pass, but due to pre-existing bugs it does not,
+    and the task does not ask us to fix bugs we find in the original code.
+    it('may not see their athletes private visibility excercise', () => {
+        login(athlete, athlete_password)
+        goToWorkout(workout_PR)
+        cy.url().as('workoutUrl');
+        login(coach, coach_password)
+        cy.get('@workoutUrl').then(url => {
+            cy.window().then(win => {
+                return win.open(url, '_self');
                 });
-            login(athlete, athlete_password)
-            goToWorkout(workout_CO)
-            cy.url().as('workoutUrl');
-            login(user, user_password)
-            cy.get('@workoutUrl').then(url => {
-                cy.window().then(win => {
-                    return win.open(url, '_self');
-                  })
-            })
-            cy.wait(500)
-            testCanNotViewDetails()
         })
-        it('may not see others private visibility excercise', () => {
-            cy.on('uncaught:exception', (err, runnable) => {
-                expect(err.message).to.include("Cannot read properties of null (reading 'owner')")
-                return false
-                });
-            login(athlete, athlete_password)
-            goToWorkout(workout_PR)
-            cy.url().as('workoutUrl');
-            login(user, user_password)
-            cy.get('@workoutUrl').then(url => {
-                cy.window().then(win => {
-                    return win.open(url, '_self');
-                  })
-            })
-            cy.wait(500)
-            testCanNotViewDetails()
-        })
+        testCanNotViewDetails()
     })
-})
+    */
+  });
+
+  describe("User", function () {
+    it("may see others public visibility excercise", () => {
+      login(user, user_password);
+      goToWorkout(workout_PU);
+      testCanViewDetails(workout_PU, "PU");
+    });
+    it("may not see others coach visibility excercise", () => {
+      cy.on("uncaught:exception", (err, runnable) => {
+        expect(err.message).to.include("Cannot read properties of null (reading 'owner')");
+        return false;
+      });
+      login(athlete, athlete_password);
+      goToWorkout(workout_CO);
+      cy.url().as("workoutUrl");
+      login(user, user_password);
+      cy.get("@workoutUrl").then((url) => {
+        cy.window().then((win) => {
+          return win.open(url, "_self");
+        });
+      });
+      cy.wait(500);
+      testCanNotViewDetails();
+    });
+    it("may not see others private visibility excercise", () => {
+      cy.on("uncaught:exception", (err, runnable) => {
+        expect(err.message).to.include("Cannot read properties of null (reading 'owner')");
+        return false;
+      });
+      login(athlete, athlete_password);
+      goToWorkout(workout_PR);
+      cy.url().as("workoutUrl");
+      login(user, user_password);
+      cy.get("@workoutUrl").then((url) => {
+        cy.window().then((win) => {
+          return win.open(url, "_self");
+        });
+      });
+      cy.wait(500);
+      testCanNotViewDetails();
+    });
+  });
+});
