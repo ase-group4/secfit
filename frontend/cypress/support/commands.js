81b7266d5f0d473d83b3bea1ddc1dc5c1da8a476
Jorunn <jorunn@selbu.org>
2022-03-20 01:16:07 +0100

    add exercise commands

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -82,6 +82,13 @@ Cypress.Commands.add("goToWorkout", (name) => {
   cy.wait(500);
 });
 
+Cypress.Commands.add("goToExercise", (name) => {
+    cy.visit("../../www/exercises.html");
+    cy.wait(500);
+    cy.contains(name).click();
+    cy.wait(500);
+  });
+
 Cypress.Commands.add("addComment", () => {
   cy.get("#comment-area").type("This is a comment.");
   cy.get("#post-comment").click();
