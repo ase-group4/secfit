c8b74d06596640122fd4b65c0a17459ab1b1f227
hermannm <hermann.morkrid@gmail.com>
2022-03-20 13:45:26 +0100

    apply formatter

--- frontend/cypress/integration/integration-tests/FR31.spec.js
+++ frontend/cypress/integration/integration-tests/FR31.spec.js
@@ -3,51 +3,49 @@
 import Chance from "chance";
 const chance = new Chance();
 
-
 describe("Integration tests", () => {
-    const user1 = chance.first() + chance.last().replace(/[^a-zA-Z0-9]/g, "-");
-    const user1_password = chance.string({ length: 10, pool: "abcd" });
-    const user2 = chance.first() + chance.last().replace(/[^a-zA-Z0-9]/g, "-");
-    const user2_password = chance.string({ length: 10, pool: "abcd" });
-    const user3 = chance.first() + chance.last().replace(/[^a-zA-Z0-9]/g, "-");
-    const user3_password = chance.string({ length: 10, pool: "abcd" });
+  const user1 = chance.first() + chance.last().replace(/[^a-zA-Z0-9]/g, "-");
+  const user1_password = chance.string({ length: 10, pool: "abcd" });
+  const user2 = chance.first() + chance.last().replace(/[^a-zA-Z0-9]/g, "-");
+  const user2_password = chance.string({ length: 10, pool: "abcd" });
+  const user3 = chance.first() + chance.last().replace(/[^a-zA-Z0-9]/g, "-");
+  const user3_password = chance.string({ length: 10, pool: "abcd" });
 
+  before(() => {
+    cy.addSimpleUser(user1, user1_password);
+    cy.addSimpleUser(user2, user2_password);
+    cy.addSimpleUser(user3, user3_password);
+  });
 
-    before(() => {
-        cy.addSimpleUser(user1, user1_password);
-        cy.addSimpleUser(user2, user2_password);
-        cy.addSimpleUser(user3, user3_password);
+  describe("FR32:The user should be able to search through users and get a list of users matching the input in the search field", () => {
+    /*
+    Checks that the users are in the userlist, and the userlist is given as list in the inputfield.
+    This automatically gives search functionality in the input field.
+    */
+    it("coaches ares member of datalist for input element", () => {
+      cy.login(user1, user1_password);
+      cy.visit("../../www/mycoach.html");
+      cy.wait(500);
+      cy.get('input[name="coach"]').should("have.attr", "list", "userlist");
+      //cy.get("#userlist").contains(`option[value=${coach}]`);
+      cy.get("#userlist").within(() => {
+        cy.get(`option[value=${user1}]`).should("not.exist");
+        cy.get(`option[value=${user2}]`);
+        cy.get(`option[value=${user3}]`);
+      });
     });
 
-    describe("FR32:The user should be able to search through users and get a list of users matching the input in the search field", () => {
-        /*Checks that the users are in the userlist, and the userlist is given as list in the inputfield.
-        This automatically gives search functionality in the input field.
-        */
-        it("coaches ares member of datalist for input element", () => {
-            cy.login(user1, user1_password)
-            cy.visit('../../www/mycoach.html')
-            cy.wait(500)
-            cy.get('input[name="coach"]').should('have.attr', 'list', 'userlist')
-            //cy.get("#userlist").contains(`option[value=${coach}]`)
-            cy.get("#userlist").within(() =>{
-                cy.get(`option[value=${user1}]`).should('not.exist');
-                cy.get(`option[value=${user2}]`)
-                cy.get(`option[value=${user3}]`)
-            })
-        });
-
-        it("athletes are members of datalist for input element", () => {
-            cy.login(user2, user2_password)
-            cy.visit('../../www/myathletes.html')
-            cy.wait(500)
-            cy.get('input[name="athlete"]').should('have.attr', 'list', 'userlist')
-            //cy.get("#userlist").contains(`option[value=${coach}]`)
-            cy.get("#userlist").within(() =>{
-                cy.get(`option[value=${user1}]`)
-                cy.get(`option[value=${user2}]`).should('not.exist');
-                cy.get(`option[value=${user3}]`)
-            })
-        });
+    it("athletes are members of datalist for input element", () => {
+      cy.login(user2, user2_password);
+      cy.visit("../../www/myathletes.html");
+      cy.wait(500);
+      cy.get('input[name="athlete"]').should("have.attr", "list", "userlist");
+      //cy.get("#userlist").contains(`option[value=${coach}]`);
+      cy.get("#userlist").within(() => {
+        cy.get(`option[value=${user1}]`);
+        cy.get(`option[value=${user2}]`).should("not.exist");
+        cy.get(`option[value=${user3}]`);
+      });
     });
-
+  });
 });
