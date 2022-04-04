1455fcf89ab6676eff61a4dc412ab2c37b5f480d
Jorunn <jorunn@selbu.org>
2022-04-04 02:32:42 +0200

    removes comment in users/admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -7,14 +7,13 @@ from .models import Offer, AthleteFile
 
 class CustomUserAdmin(UserAdmin):
     """
-    Class that extends the default admin view for User by adding a
+    A class that extends the default admin view for User by adding a
     coach field to the create-view and the edit-view
     """
 
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = get_user_model()
-    # list_display = UserAdmin.list_display + ('coach',)
     fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("coach",)}),)
     add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("coach",)}),)
 
