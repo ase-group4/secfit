c9ba4429eae24414a89458323f23168b152aeb34
Jorunn <jorunn@selbu.org>
2022-04-04 02:23:06 +0200

    adds newline after docstring

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,7 +6,8 @@ from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 
 class CustomUserAdmin(UserAdmin):
-    """ Class that extends the default admin view for User by adding a coach field to the create-view and the edit-view """
+    """Class that extends the default admin view for User by adding a coach field to the create-view and the edit-view"""
+
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = get_user_model()
