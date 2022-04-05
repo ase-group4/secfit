6c7eb815bd667878604c55142d4207979710a237
hermannm <hermann.morkrid@gmail.com>
2022-04-05 10:23:30 +0200

    fix static url patterns

--- backend/secfit/urls.py
+++ backend/secfit/urls.py
@@ -20,16 +20,18 @@ from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from secfit import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("workouts.urls")),
-    path("", include("meals.urls")),
-    path("", include("users.urls")),
-    path("", include("comments.urls")),
-    path("api/auth/", include("rest_framework.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("", views.api_root),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-]
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("workouts.urls")),
+        path("", include("meals.urls")),
+        path("", include("users.urls")),
+        path("", include("comments.urls")),
+        path("api/auth/", include("rest_framework.urls")),
+        path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+        path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+        path("", views.api_root),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
