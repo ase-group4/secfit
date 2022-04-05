5106a00bdb3e87189bd5e415b24aa7753d7a2ecb
Jorunn <jorunn@selbu.org>
2022-04-05 21:44:48 +0200

    refactors comments

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from comments.views import CommentList, CommentDetail, LikeList, LikeDetail
+from comments.views import CommentList, CommentDetail
 
 urlpatterns = [
     path("api/comments/", CommentList.as_view(), name="comment-list"),
     path("api/comments/<int:pk>/", CommentDetail.as_view(), name="comment-detail"),
-    path("api/likes/", LikeList.as_view(), name="like-list"),
-    path("api/likes/<int:pk>/", LikeDetail.as_view(), name="like-detail"),
 ]
